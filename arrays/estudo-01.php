c307d06103ec71b4d1830bd6059038cb90e7efac
Jhonathan Sinzervch <jhonathan.con@outlook.com>
2018-07-27 15:34:17 -0300

    inclusao de novas aulas

---
+++ arrays/estudo-01.php
@@ -0,0 +1,20 @@
+<?php
+
+	$pessoas = array();
+
+	array_push($pessoas, array(
+		0 => 'Joao',
+		1 => '15',
+		2 => 'Programador'
+	));
+
+	array_push($pessoas, array(
+		0 => 'Marcos',
+		1 => '20',
+		2 => 'Barista'
+	));
+
+
+	print_r($pessoas);
+
+?>
